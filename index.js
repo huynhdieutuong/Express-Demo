ca050dba1cb6bcc3254b09b1bd201d568f283b79
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2019-07-05 20:50:08 +0700

    3-query-parameters

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const users = [
+  { id: 1, name: 'Thinh' },
+  { id: 2, name: 'Hung' },
+  { id: 3, name: 'Tuong' }
+];
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-app.get('/', (req, res) => res.render('index', { name: 'Tuong'}));
-app.get('/users', (req, res) => res.render('users/index', { users: [
-  { id: 1, name: 'Thinh' },
-  { id: 2, name: 'Hung' },
-  { id: 3, name: 'Tuong' }
-]}))
+app.get('/', (req, res) => res.render('index', { name: 'Tuong' }));
+app.get('/users', (req, res) => res.render('users/index', { users }));
+app.get('/users/search', (req, res) => {
+  const { q } = req.query;
+  const filtered = users.filter(user => user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1);
+  res.render('users/index', { users: filtered, value: q });
+})
 
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
