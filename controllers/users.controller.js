44579a7355a7cc401bfd9bd10a8ce7e8efeee30d
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2019-07-08 17:13:55 +0700

    12-server-side-validation

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -14,6 +14,17 @@ module.exports.search = (req, res) => {
 
 module.exports.create = (req, res) => res.render('users/create');
 module.exports.postCreate = (req, res) => {
+  let errors = [];
+  const { name, phone } = req.body;
+  if(!name) {
+    errors.push('Name is empty');
+  }
+  if(!phone) {
+    errors.push('Phone is empty');
+  }
+  if(errors.length) {
+    return res.render('users/create', { errors, values: req.body });
+  }
   req.body.id = uuid();
   users.push(req.body);
   db.write();
